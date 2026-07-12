03cd243e2cdeec51b674c6199d1c57310bb43626
Toni Elias <33258055+toni1elias@users.noreply.github.com>
2026-07-12 15:13:58 +0200

    Create 03 Matrix Calculator.py

---
+++ 03 Matrix Calculator.py
@@ -0,0 +1,13 @@
+import numpy as np
+A = np.matrix([[2,-3,0],[1,5,19]])
+print(np.dot(4,A))
+A = np.matrix([[5,1,3]])
+B = np.matrix([[2],[4],[6]])
+print(np.dot(A,B))
+A = np.matrix([[1,3,2],[4,0,-1]])
+B = np.matrix([[5,10],[2,-2],[-3,6]])
+print(np.dot(A,B))
+print(A*B)
+A = np.matrix([[3],[2]])
+B = np.matrix([[5],[4]])
+print(np.kron(A,B))
