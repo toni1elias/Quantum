f5489b954892c84e9028da93ea28801a6b62c610
Toni Elias <33258055+toni1elias@users.noreply.github.com>
2026-07-12 15:11:46 +0200

    Update 01 Setup and Test.py

--- 01 Setup and Test.py
+++ 01 Setup and Test.py
@@ -1,6 +1,6 @@
 from qiskit_ibm_runtime import QiskitRuntimeService
 QiskitRuntimeService.save_account(
-token="uyJAsZ9pQgfgJRPJKPQ3TrKZUVzYHnEo5CX27Y7iLXHd",
+token="",
 instance="crn:v1:bluemix:public:quantum-computing:us-east:a/ea7adc82e6024e3fab8d609be7a24004:d805ce05-20b8-4f05-9c12-275e85465a47::",
 overwrite=True
 )
